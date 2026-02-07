e4b5a3c38127639eeb0f1193960da8feddf42a9a
kalahasthimanikanata <manikanta7973@gmail.com>
2026-02-07 13:06:35 +0530

    chanbges

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
+
+export default App;
